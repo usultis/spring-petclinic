afb4b6e60bdc24a11eae6e533c40f7bd211d819b
usul <usultis@gmail.com>
2019-10-08 00:36:11 +0100

    making skaffold happy

--- watch.sh
+++ watch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # watch the java files and continously deploy the service
-mvn clean install
+mvn -DskipTests=true clean install
+export UUID=$(uuidgen)
 skaffold run -p dev
-reflex -r "\.java$" -- bash -c 'mvn install && skaffold run -p dev'
+reflex -r "\.java$" -r "\.html$" -- bash -c 'export UUID=$(uuidgen) && mvn -DskipTests=true install && skaffold run -p dev'
