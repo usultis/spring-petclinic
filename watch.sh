f3f9d48bb42dd3dade9bf95b9230cb5fb044106d
usul <usultis@gmail.com>
2019-10-08 02:14:13 +0100

    jib

--- watch.sh
+++ watch.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # watch the java files and continously deploy the service
-mvn -DskipTests=true clean install
 export UUID=$(uuidgen)
 skaffold run -p dev
-reflex -r "\.java$" -- bash -c 'export UUID=$(uuidgen) && mvn -DskipTests=true install && skaffold run -p dev'
+reflex -r "\.java$" -- bash -c 'export UUID=$(uuidgen) && skaffold run -p dev'
